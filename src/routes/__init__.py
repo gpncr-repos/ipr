14cb057220be2fa692659b760b3822e5b0f78522
AlexeyVodopyan <pa_to_atm>
2022-07-27 09:22:56 +0300

    feat: добавлены файлы сервиса

---
+++ src/routes/__init__.py
@@ -0,0 +1,11 @@
+from src.calculations.vogel_ipr import calc_ipr
+from fastapi import APIRouter
+from src.models.models import IprCalcRequest, IprCalcResponse
+
+main_router = APIRouter(prefix="/ipr", tags=["IPR"])
+
+
+@main_router.post("/calc", response_model=IprCalcResponse)
+async def my_profile(ipr_in: IprCalcRequest):
+    result = IprCalcResponse.parse_obj(calc_ipr(**ipr_in.dict()))
+    return result

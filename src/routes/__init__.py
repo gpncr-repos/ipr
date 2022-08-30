471a4ee15e9b86da68497ca545226776da2bad17
ShamRail <rail1999g102r@mail.ru>
2022-08-30 15:23:36 +0300

    feat: добавлен вызов расчета IPR в endpoint
    #1

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from src.models.models import IprCalcRequest, IprCalcResponse
+from src.calculations.vogel_ipr import calc_ipr
 
 main_router = APIRouter(prefix="/ipr", tags=["IPR"])
 
@@ -7,5 +8,4 @@ main_router = APIRouter(prefix="/ipr", tags=["IPR"])
 @main_router.post("/calc", response_model=IprCalcResponse)
 async def my_profile(ipr_in: IprCalcRequest):
     """Эндпоинт расчёта IPR"""
-    from src.calculations.vogel_ipr import calc_ipr
-    pass
+    return calc_ipr(ipr_in.p_res, ipr_in.pi, ipr_in.wct, ipr_in.pb)

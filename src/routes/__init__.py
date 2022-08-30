e8fe010140b0991f96848e552369e464afa7fdc4
masalimov.tr <mstimur941@gmail.com>
2022-08-30 15:39:41 +0300

    feat: Добавлен endpoint расчета IPR
    closes: #21

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter
 from src.models.models import IprCalcRequest, IprCalcResponse
+from src.calculations.vogel_ipr import calc_ipr
 
 main_router = APIRouter(prefix="/ipr", tags=["IPR"])
 
 
 @main_router.post("/calc", response_model=IprCalcResponse)
-async def my_profile(ipr_in: IprCalcRequest):
-    """Эндпоинт расчёта IPR"""
-    from src.calculations.vogel_ipr import calc_ipr
-    pass
+def post_ipr(ipr_in: IprCalcRequest):
+    """
+    Эндпоинт расчёта IPR
+
+    :param ipr_in - модель входных параметров пласта
+    """
+    ipr = calc_ipr(ipr_in.p_res, ipr_in.pi, ipr_in.wct, ipr_in.pb)
+    ipr_response = IprCalcResponse(**{
+        "q_liq": ipr["q_liq"],
+        "p_wf": ipr["p_wf"]
+    })
+    return ipr_response
